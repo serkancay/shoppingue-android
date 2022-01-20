fbc8e17ef5deb2c517c034aef1ee5e9279476119
serkancay <sserkancay@gmail.com>
2022-01-20 23:33:37 +0300

    Implement bottom navigation view

--- app/src/main/java/com/serkancay/shoppingue/MainActivity.kt
+++ app/src/main/java/com/serkancay/shoppingue/MainActivity.kt
@@ -3,6 +3,9 @@ package com.serkancay.shoppingue
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
